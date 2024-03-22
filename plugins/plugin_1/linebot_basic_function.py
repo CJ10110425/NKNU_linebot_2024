069d6d9c0756f30aa93fdfce47826392fab893f7
李品澤 <ze0966747312@gmail.com>
2024-03-22 16:54:19 +0800

    feature: finish linebot_basic_function

---
+++ plugins/plugin_1/linebot_basic_function.py
@@ -0,0 +1,32 @@
+# linebot-_basic_function.py 製作 linebot 擁有基本的功能
+# 主要功能為:
+# 1. 回覆使用者訊息
+# 2. 主動推播訊息
+
+
+from linebot.models import (
+    TextSendMessage
+)
+
+
+class LineBotBasicFunction():
+    def __init__(self, line_bot_api, event):
+        self.line_bot_api = line_bot_api
+        self.event = event
+        self.user_id = event.source.user_id
+        self.message = event.message.text
+    
+    def reply_message(self, message=str):
+        self.line_bot_api.reply_message(
+            self.event.reply_token,
+            TextSendMessage(text=message)
+        )
+
+    # 使用者可以輸入 user_id 來推播訊息或是直接使用預設的 self_user_id 推播訊息
+    def push_message(self, message=str, user_id=None):
+        if user_id:
+            self.line_bot_api.push_message(
+                user_id, TextSendMessage(text=message))
+        else:
+            self.line_bot_api.push_message(
+                self.user_id, TextSendMessage(text=message))
